248ce453a30f379bc74c3890fa3d87a1a1885c6c
Yael Galifat <yaelutra@gmail.com>
2020-05-14 19:05:09 +0300

    adding a feature brnach like i am on a team

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 	<p>
-	 I am adding a new line to this shit i`m such a web developer 
+	 I am adding a new line to this shit i`m such a web developer !!!!
         </p>
         <a
           className="App-link"
